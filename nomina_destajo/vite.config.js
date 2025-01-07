fee6014b270e318ed41d09588dc4879709e58590
msolis-cs <msolis@corpsoftsa.com>
2025-01-07 14:04:44 -0600

    Config Port

--- nomina_destajo/vite.config.js
+++ nomina_destajo/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+/*
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+*/
+
+export default defineConfig({
+  server: {
+    port: process.env.VITE_PORT || 3000,  // Usa el valor de la variable de entorno, o 3000 como predeterminado
+  },
+});
